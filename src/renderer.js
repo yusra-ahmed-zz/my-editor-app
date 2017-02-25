b4aafb205d2aa1218c69383b6a2118925c4600b0
Yusra Ahmed <yusra.ahmed05@gmail.com>
2017-02-25 13:55:08 -0800

    created editor app

---
+++ src/renderer.js
@@ -0,0 +1,25 @@
+const { ipcRenderer } = require('electron');
+const loader = require('monaco-loader');
+const fs = require('fs');
+
+
+loader().then((monaco) => {
+  let editor = monaco.editor.create(document.getElementById('container'), {
+    language: 'javascript',
+    theme: 'vs-dark',
+    automaticLayout: true
+  });
+  ipcRenderer.on('navigate', (e, url) => {
+      url = url.slice(7);
+      console.log(`Trying to read file from ${url}`);
+
+      fs.readFile(url, 'utf8', (error, result) => {
+          if (!error) {
+              console.log(result);
+
+              editor.setModel(monaco.editor.createModel(result, 'javascript'));
+          }
+      });
+  });
+});
+
